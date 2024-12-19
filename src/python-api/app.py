7fbe4ee75355b76cdac34a6a60f90897b3e1c204
Julian Walston <julianrwalston@gmail.com>
2024-12-19 15:29:19 -0600

    updated API to accept comma separated string of skills

--- src/python-api/app.py
+++ src/python-api/app.py
@@ -14,12 +14,10 @@ def create_app(db_username, db_password):
     CORS(server)
     api = Api(server)
 
-    print("Connecting to database...")
     # Connect to MongoDB database
     database = get_database(db_username, db_password) 
     course_collection = database["Learnify Courses"]
 
-    print(list(course_collection.find()))
     #create api blueprint
     courses_bp = Blueprint("Courses", "courses", url_prefix="/courses", description="Online CS Courses API")
 
@@ -32,7 +30,16 @@ def create_app(db_username, db_password):
         Return courses that match parameters."""
 
         try:
-            data = {"results": list(course_collection.find(args))}
+            data = {"results": []}
+            if len(args) == 0:
+                data["results"] = list(course_collection.find())
+            else:
+                # accept multiple skills as comma separated string
+                skills = args["skills"].split(",")
+                skills_query = {"$or": []}
+                for skill in skills:
+                    skills_query["$or"].append({"skills": skill})
+                data["results"] = list(course_collection.find(skills_query))
 
             for entry in data["results"]: 
                 entry["_id"] = str(entry["_id"]) # stringify ObjectIds
@@ -61,46 +68,3 @@ def create_app(db_username, db_password):
     api.register_blueprint(courses_bp)
 
     return server
-
-# from flask_cors import CORS
-
-# def create_app(db_username, db_password):
-#     server = Flask(__name__)
-#     CORS(server)  # Enable CORS for all routes
-#     server.config.from_object(APIConfig)
-#     api = Api(server)
-
-#     # MongoDB setup
-#     database = get_database(db_username, db_password) 
-#     course_collection = database["Learnify Courses"]
-
-#     # API Blueprint
-#     courses_bp = Blueprint("Courses", "courses", url_prefix="/courses", description="Online CS Courses API")
-
-#     @courses_bp.route("/")
-#     @courses_bp.arguments(CourseQueryArgsSchema, location="query")
-#     @courses_bp.response(200, CourseSchema(many=True))
-#     def get_courses(args):
-#         """Find courses"""
-#         try:
-#             data = {"results": list(course_collection.find(args))}
-
-#             for entry in data["results"]: 
-#                 entry["_id"] = str(entry["_id"])  # Convert ObjectId to string
-
-#             return bson_to_json(data)
-#         except TypeError:
-#             abort(400, message="Query arguments are not in the correct format")
-
-#     @courses_bp.route("/<course_id>")
-#     @courses_bp.response(200, CourseSchema)
-#     def get_course_by_id(course_id):
-#         """Find course by ID"""
-#         course = course_collection.find_one({"_id": ObjectId(course_id)})
-#         if course is None:
-#             abort(404, message="Course not found.")
-#         course["_id"] = str(course["_id"])  # Convert ObjectId to string
-#         return bson_to_json(course)
-
-#     api.register_blueprint(courses_bp)
-#     return server
